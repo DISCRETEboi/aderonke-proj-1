61e1b9197b49716ac63555ab2492db00ec0e9d4d
Abdul-Mateen Qamardeen <matdboi@gmail.com>
2023-11-16 18:18:30 +0100

    yes

--- 500666461.R
+++ 500666461.R
@@ -1,35 +1,21 @@
----
-title: "Data Analysis using R"
-author: '******'
-date: "`r Sys.Date()`"
-output: pdf_document
----
+### date: Nov. 16, 2023
+### student id: 500666461
+### program purpose: data analysis using R
 
-Loading required libraries *dplyr*, *stringr* and *ggplot2*
-
-```{r}
+## loading required libraries *dplyr*, *stringr* and *ggplot2*
 library(dplyr)
 library(stringr)
 library(ggplot2)
-```
 
-## Task 1
-
-Reading the dataset into R
-
-```{r}
+### Task 1
+## reading the dataset into R
 cdrc_data <- read.csv("hh_churn_lsoa11_2023.csv")
 str(cdrc_data) #extraneous
-```
 
-## Task 2
-
-Examining the LSOA code W01000092 Menai (Bangor), and identifying the residential mobility indices which are greater than 0.5 and less than 0.8.
-
-```{r}
+### Task 2
+## examining the LSOA code W01000092 Menai (Bangor), and identifying the residential mobility indices which are greater than 0.5 and less than 0.8.
 cdrc_data_menai_df <- filter(cdrc_data, area == "W01000092")
 cdrc_data_menai <- unlist(cdrc_data_menai_df[1, -1])
-
 cdrc_data_menai_sub <- cdrc_data_menai[cdrc_data_menai > 0.5 & cdrc_data_menai < 0.8]
 # printing the named vector
 print(cdrc_data_menai_sub)
@@ -38,25 +24,17 @@ print(names(cdrc_data_menai_sub))
 # printing the actual years
 x <- str_replace(names(cdrc_data_menai_sub), "chn", "")
 print(x)
-```
 
-## Task 3
-
-Appending additional columns to the data
-
-```{r}
+### Task 3
+## appending additional columns to the data
 # adds column one (averaged residential mobility for each region) as ARMI
 cdrc_data$ARMI <- round(rowMeans(cdrc_data[, -c(1, 28)]), 3)
 # adds column two (grouping regions into low, medium and high) as ARMIgrpd
 cdrc_data$ARMIgrpd <- cut(cdrc_data$ARMI, breaks = c(0, 0.2, 0.5, 1), labels = c("Low", "Medium", "High"))
 str(cdrc_data)
-```
 
-## Task 4
-
-xxx
-
-```{r}
+### Task 4
+## First insight (with chart)
 avg_armi <- round(colMeans(cdrc_data[, 2:27]), 3)
 years <- as.numeric(str_replace(names(avg_armi), "chn", ""))
 ts_df <- data.frame(year = years, avg_rmi = avg_armi)
@@ -67,32 +45,11 @@ ggplot(ts_df, aes(x = year, y = avg_rmi)) +
        y = "Average residential mobility index",
        title = "Trend of residential mobility index over time across regions") +
   theme_bw()
-```
 
-The chart in Figure is a time series plot showing the change in residential mobility index (averaged across all the regions) over time from 1997 to 2022. The plot clearly shows a sharp downward trend in the residential mobility index over time. 
-
-
-```{r}
-ggplot(cdrc_data, aes(x = chn1997, y = chn2022, color = ARMIgrpd)) +
-  geom_point() +
-  labs(x = "Residential mobility index in 1997",
-       y = "Residential mobility index in 2022",
-       color = "RMI level",
-       title = "Correlation plot for the residential mobility index - year 2022 against 1997") +
-  theme_bw()
-```
-
-
-
-
-
-
-
-
-
-
-
-
+# The chart in Figure 1 is a time series plot showing the change in residential mobility index (averaged across all the regions) over time
+# from 1997 to 2022. The plot clearly shows a sharp downward trend in the residential mobility index over time. The downward tredn is also
+# observed to be uniform, with the line approximately straight. The inference from the chart is that, on average, as years pass by, the residential
+# mobility decreases. And this applies to all regions.
 
 
 
